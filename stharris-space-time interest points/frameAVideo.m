3e1aa3d794484ab8efa1200284fdc33792b70f88
Harindu Ashan <harinduashan@gmail.com>
2018-05-23 21:17:45 +0530

    Add the working STIP code, name as stharris-stip.

---
+++ stharris-space-time interest points/frameAVideo.m
@@ -0,0 +1,36 @@
+% ------------------------------------------------------------ %
+% @func - frameAVideo(name_prefix, format, i1, i2)
+% @info - read a video, frame it, and converts their values to
+%         (double)grey and returns a 3D x-y-t image array.
+% @var - name_prefix: Name of the video
+%        format: Video supported formats
+%                   '.avi', '.mp4'
+% @output - 3D matrix with pixel values of the defined images
+% ------------------------------------------------------------ %
+function result = frameAVideo(name_prefix, format)
+
+% ------------------------------------------------------------ %
+% Framing the Video
+% ------------------------------------------------------------ %
+
+if strcmp(format, 'avi')
+    videoInfo = VideoReader(sprintf('%s.avi',name_prefix));
+elseif strcmp(format, 'mp4')
+    videoInfo = VideoReader(sprintf('%s.amp4',name_prefix));
+else
+    warning('Unsupported Video file format..');
+    return;
+end
+
+% Number of frames for a video to be broken
+numFrames = videoInfo.NumberOfFrames; % TODO: Need to find the framing parameter and algorithm
+
+% To create the result matrix
+f0 = il_rgb2gray(double(read(videoInfo, 1)));
+result = zeros([size(f0), numFrames]);
+
+for iFrame=1:numFrames
+    result(:,:,iFrame)=il_rgb2gray(double(read(videoInfo, iFrame)));
+end
+
+
--- stharris-space-time interest points/il_rgb2gray.m
+++ stharris-space-time interest points/il_rgb2gray.m
@@ -1,14 +1,14 @@
-function gray=il_rgb2gray(rgb)
+% ------------------------------------------------------------ %
+% @func - il_rgb2gray(rgb)
+% @info - converts n-channel image into a one channel
+% @var - rgb: Input image. This may be either color image or
+%             a gray image.
+% @output - matrix of pixel values within 0 -255 scale
+% ------------------------------------------------------------ %
+function gray = il_rgb2gray(rgb)
 
-%
-% gray=il_rgb2gray(rgb)
-%
-%   converts n-channel image into a one channel
-%   (gray) image as gray=(r+g+b+...)/n
-%
-
-if ndims(rgb)<3
-  gray=rgb;
+if ismatrix(rgb)
+  gray = rgb;
 else
-  gray=sum(rgb,3)/size(rgb,3);
+  gray = sum(rgb,3)/size(rgb,3);
 end
